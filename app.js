8401ed10a20e7da3657dfa28bb289cf37d82be55
ta0chen <taochen326@gmail.com>
2016-07-07 15:47:33 +0800

    upload source code

---
+++ app.js
@@ -0,0 +1,23 @@
+/**
+ * Created by tachen on 7/7/2016.
+ */
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//configure app
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// use middleware
+app.use(bodyParser());
+
+//define routes
+app.use(require('./todos'));
+
+//start the server
+app.listen(8787, function () {
+    console.log('ready on port 8787');
+});
